f372017fb7b67efe4ac234270dd07f6702c48e39
Bunny5942 <kazifarzan23@gmail.com>
2024-06-01 15:56:54 +0530

    checkpoint removing hero section animation

--- gsap-heroSection.js
+++ gsap-heroSection.js
@@ -6,24 +6,25 @@ navTl.to("#page1 .nav-bar, #page1 .nav-item, #page1 .nav-bar .vertical-ruler ", 
 
 // Make BEP Smaller and move it up 
 navTl.to(" .hero-title1 ", {
+    
     duration: 1,
     scale: .2,
-    y:"-40%",
+    y:"-10%",
     ease: "power1.out",
   });  
 
 //Turn the page into a nav bar
 navTl.to("#page1",{ 
-    position: "fixed",
+    position: "fixed",    
     height:"10%",
     duration:1,
 }, "-=1");
 
 //disappear subtitle, buttons
 navTl.to("#page1 .title-container .hero-title2, #page1 .button-container .guideline-button1, #page1 .button-container .guideline-button2 ", {
-    duration: .05,
-    opacity:0,
-    y:"-90%",
+    duration: .5,
+    opacity:0,   
+
   }, "-=1");
 
   //Move BEP to the left
@@ -33,10 +34,16 @@ navTl.to("#page1 .title-container .hero-title1 ", {
     ease: "power1.out"
   }, );
 
+//turn the nav column to a row
+navTl.to("#page1",{     
+    flexDirection:"row",
+    justifyContent: "space-between",    
+},);
+
 //nav-bar reappears
 navTl.to("#page1 .nav-bar, #page1 .nav-bar .vertical-ruler ", { opacity:1, duration:.2 });
 
-navTl.to("#page1 .nav-bar", { opacity:1, duration:.25, bottom: "25%", }, "-=0.5");
+navTl.to("#page1 .nav-bar", { opacity:1, duration:.25, bottom: "25%", left: "35%"}, "-=0.5");
 
 navTl.to("#page1 .nav-item ", { opacity:1,stagger:0.1, duration:.2 ,y:9 });
 
@@ -45,9 +52,9 @@ navTl.to("#page .button-container ", {
     duration:.2,    
 });
 
-navTl.to("#page1 .button-container .guideline-button1 ", { opacity:1, duration:0.2,ease: "power1.out", scale:0.5});
+navTl.to("#page1 .button-container .guideline-button1 ", { opacity:1, duration:0.4, scale:0.8});
 
-navTl.to("#page1 .button-container .guideline-button2 ", { opacity:1, duration:0.2 ,ease: "power1.out", scale: 0.5});
+navTl.to("#page1 .button-container .guideline-button2 ", { opacity:1, duration:0.4 , scale: 0.8},"-=0.4");
 
 
 
